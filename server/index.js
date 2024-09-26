5ed43c359dcc3ec7904906543e56793a97315e3e
aasupa <60641204+aasupa@users.noreply.github.com>
2024-09-26 14:46:37 +0545

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,7 +28,8 @@ app.use(express.json())
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(cors({ origin: 'https://deploy-data-share.vercel.app' },
-            methods:['POST', 'GET'],
+            methods:['POST', 'GET', 'PUT', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
             credentials: true
             ));
 
