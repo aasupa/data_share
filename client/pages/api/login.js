26d4b361ac4bf6bed39c875f6d80f70efe754d97
aasupa <60641204+aasupa@users.noreply.github.com>
2024-09-26 13:08:45 +0545

    Update login.js

--- client/pages/api/login.js
+++ client/pages/api/login.js
@@ -1,6 +1,6 @@
 // Example: pages/api/login.js
 import axios from 'axios';
-
+axios.default.withCredentials =true;
 export default async function handler(req, res) {
   try {
     const response = await axios.post('http://deploy-data-share.vercel.app/api/login', req.body);
