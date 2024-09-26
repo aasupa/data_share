9213a27e882b3305673e096d7b998332b1e83980
aasupa <60641204+aasupa@users.noreply.github.com>
2024-09-26 16:10:24 +0545

    Update datasetRoutes.js

--- server/routes/datasetRoutes.js
+++ server/routes/datasetRoutes.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import { getDatasetById, getAllDatasetIds, uploadDataset, updateDataset } from '../controller/datasetController.js';
-import multer from 'multer';
 
-const upload = multer({ dest: 'uploads/' });
+
+
 const router = express.Router();
 
 
 // API to get a dataset by its ID
 router.get('/dataset/:datasetId', getDatasetById);
 router.get('/datasets/ids', getAllDatasetIds);
-router.post('/dataset/upload-dataset',upload.single('dataset'), uploadDataset);
-router.put('/dataset/update-dataset/:datasetId', upload.single('dataset'), updateDataset);
+
 export default router;
