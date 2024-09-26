fcab195a8b0c536f223334c22240c9e1ad8b3342
aasupa <60641204+aasupa@users.noreply.github.com>
2024-09-26 13:05:30 +0545

    Update login.js

--- client/pages/api/login.js
+++ client/pages/api/login.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 export default async function handler(req, res) {
   try {
-    const response = await axios.post('http://localhost:3001/api/login', req.body);
+    const response = await axios.post('http://deploy-data-share.vercel.app/api/login', req.body);
     res.status(response.status).json(response.data);
   } catch (error) {
     res.status(error.response?.status || 500).json(error.response?.data || { error: 'Server error' });
